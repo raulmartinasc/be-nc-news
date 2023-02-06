ce3a28b80854b04fcef26b3ec4b68a92b0fa5547
raulmartinasc <raulmartinascanio@gmail.com>
2023-02-06 10:02:07 +0000

    allow cross origin resource sharing

--- db/app.js
+++ db/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const {
   getAllPaths,
   getTopics,
@@ -11,6 +12,7 @@ const {
   deleteComment,
   addVote,
 } = require("../db/controller");
+app.use(cors());
 app.use(express.json());
 app.get("/api", getAllPaths);
 app.get("/api/topics", getTopics);
